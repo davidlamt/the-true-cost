cb3efe01acacf917efea5510bc377bd656a46b66
David Tran <davidlamt@gmail.com>
2020-06-10 20:17:05 -0700

    Update more configurations and remove CRA template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './Header';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FunctionComponent = () => {
+  return (
+    <header>
+      <h1>The True Cost</h1>
+    </header>
+  );
+};
+
+export default Header;
