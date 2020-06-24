ce6b02d706bfc3fbc2fa4dc9843c35f1d5477dc4
David Tran <davidlamt@gmail.com>
2020-06-23 21:47:13 -0700

    Updates

---
+++ precache-manifest.359bcfc54ea3b1df12a2b8efd0ac30ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba0c30bc8ee4eeb6598eb145f94a2e7",
+    "url": "/the-true-cost/index.html"
+  },
+  {
+    "revision": "b97da1fe7e7dc81f25ac",
+    "url": "/the-true-cost/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "555a19564b8ad6096932",
+    "url": "/the-true-cost/static/js/2.13113bd7.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/the-true-cost/static/js/2.13113bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97da1fe7e7dc81f25ac",
+    "url": "/the-true-cost/static/js/main.3a3884e7.chunk.js"
+  },
+  {
+    "revision": "6d2b95dea783eeaaa3b1",
+    "url": "/the-true-cost/static/js/runtime-main.5551671e.js"
+  },
+  {
+    "revision": "530abe7ff103dccf29f56d77366f24b5",
+    "url": "/the-true-cost/static/media/antenna-dish.530abe7f.svg"
+  },
+  {
+    "revision": "cf5153e483379a71e17da34467485620",
+    "url": "/the-true-cost/static/media/arm-dumbbell.cf5153e4.svg"
+  },
+  {
+    "revision": "c6145839bf20d3d1f4676bebb0578ef1",
+    "url": "/the-true-cost/static/media/car.c6145839.svg"
+  },
+  {
+    "revision": "702cace23e234989bd17355f5cc5e328",
+    "url": "/the-true-cost/static/media/coffee.702cace2.svg"
+  },
+  {
+    "revision": "dc1970f6b799a5e69d93f98424c0a19f",
+    "url": "/the-true-cost/static/media/phone.dc1970f6.svg"
+  },
+  {
+    "revision": "93476a7d1c2202c413da1d1fce481ee2",
+    "url": "/the-true-cost/static/media/reset.93476a7d.svg"
+  }
+]);
